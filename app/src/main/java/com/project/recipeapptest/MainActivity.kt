9294fe0aea52fe939a7e544b701d6a9d6efd7156
EhsanTgv <EhsanTgv@gmail.com>
2021-08-10 02:04:28 +0430

    Implement Columns in view

--- app/src/main/java/com/project/recipeapptest/MainActivity.kt
+++ app/src/main/java/com/project/recipeapptest/MainActivity.kt
@@ -3,36 +3,64 @@ package com.project.recipeapptest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.recipeapptest.ui.theme.RecipeAppTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            RecipeAppTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color(0xFFF2F2F2))
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.happy_meal_small),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth(),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Happy Meal",
+                        fontSize = 26.sp
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "800 Calories",
+                        fontSize = 17.sp
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$5.99",
+                        fontSize = 17.sp,
+                        color = Color(0xff85bb65)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RecipeAppTestTheme {
-        Greeting("Android")
     }
 }
