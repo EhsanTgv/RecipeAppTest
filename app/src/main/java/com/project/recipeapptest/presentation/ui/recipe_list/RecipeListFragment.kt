06a88a3adb6927e0508ebb77c98186b53b019036
EhsanTgv <EhsanTgv@gmail.com>
2021-08-25 23:21:14 +0430

    RecipeListFragment add Scaffold

--- app/src/main/java/com/project/recipeapptest/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/project/recipeapptest/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -51,36 +52,39 @@ class RecipeListFragment : Fragment() {
 
                     val loading = viewModel.loading.value
 
-                    Column(
-                        modifier = Modifier.background(MaterialTheme.colors.background)
-                    ) {
-                        SearchAppBar(
-                            query = query,
-                            onQueryChanged = viewModel::onQueryChanged,
-                            onExecuteSearch = viewModel::newSearch,
-                            selectedCategory = selectedCategory,
-                            onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
-                            onToggleTheme = { application.toggleTheme() }
-                        )
+                    Scaffold(
+                        topBar = {
+                            SearchAppBar(
+                                query = query,
+                                onQueryChanged = viewModel::onQueryChanged,
+                                onExecuteSearch = viewModel::newSearch,
+                                selectedCategory = selectedCategory,
+                                onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
+                                onToggleTheme = { application.toggleTheme() }
+                            )
+                        },
+                        content = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colors.background)
+                            ) {
+                                if (loading) {
+                                    LoadingRecipeListShimmer(250.dp)
 
-                        Box(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            if (loading) {
-                                LoadingRecipeListShimmer(250.dp)
-
-                            } else {
-                                LazyColumn {
-                                    itemsIndexed(
-                                        items = recipes
-                                    ) { index, recipe ->
-                                        RecipeCard(recipe = recipe, onClick = {})
+                                } else {
+                                    LazyColumn {
+                                        itemsIndexed(
+                                            items = recipes
+                                        ) { index, recipe ->
+                                            RecipeCard(recipe = recipe, onClick = {})
+                                        }
                                     }
                                 }
+                                CircularIndeterminateProgressBar(isDisplayed = loading)
                             }
-                            CircularIndeterminateProgressBar(isDisplayed = loading)
                         }
-                    }
+                    )
                 }
             }
         }
