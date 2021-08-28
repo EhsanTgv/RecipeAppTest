16d3ca2052604f941a2c1f4c923589d27db85f37
EhsanTgv <EhsanTgv@gmail.com>
2021-08-28 14:25:49 +0430

    implement design in RecipeFragment

---
+++ app/src/main/java/com/project/recipeapptest/presentation/components/RecipeView.kt
@@ -0,0 +1,100 @@
+package com.project.recipeapptest.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.project.recipeapptest.domin.model.Recipe
+import com.project.recipeapptest.util.DEFAULT_RECIPE_IMAGE
+import com.project.recipeapptest.util.loadPicture
+
+const val IMAGE_HEIGHT = 260
+
+@Composable
+fun RecipeView(
+    recipe: Recipe,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.background)
+    ) {
+        item {
+            recipe.featureImage?.let { url ->
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img ->
+                    Image(
+                        bitmap = img.asImageBitmap(),
+                        contentDescription = "Recipe Image",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(IMAGE_HEIGHT.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        8.dp
+                    )
+            ) {
+                recipe.title?.let { title ->
+                    Row(
+                        modifier = Modifier.padding(
+                            bottom = 8.dp,
+                        )
+                    ) {
+                        Text(
+                            text = title,
+                            modifier = Modifier
+                                .fillMaxWidth(0.85f)
+                                .wrapContentWidth(Alignment.Start),
+                            style = MaterialTheme.typography.h3
+                        )
+                        Text(
+                            text = recipe.rating.toString(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth(Alignment.End)
+                                .align(Alignment.CenterVertically),
+                            style = MaterialTheme.typography.h5
+                        )
+                    }
+                }
+                recipe.publisher?.let { publisher ->
+                    val updated = recipe.dateUpdated
+                    Text(
+                        text = if (updated != null) {
+                            "Updated $updated by $publisher"
+                        } else {
+                            "By $publisher"
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+                for (ingredient in recipe.ingredients) {
+                    Text(
+                        text = ingredient,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/project/recipeapptest/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/project/recipeapptest/presentation/ui/recipe/RecipeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
@@ -14,7 +18,12 @@ import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.project.recipeapptest.presentation.BaseApplication
+import com.project.recipeapptest.presentation.components.CircularIndeterminateProgressBar
+import com.project.recipeapptest.presentation.components.RecipeView
+import com.project.recipeapptest.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecipeFragment : Fragment() {
@@ -22,6 +31,9 @@ class RecipeFragment : Fragment() {
     private val viewModel: RecipeViewModel by viewModels()
 
     private val args: RecipeFragmentArgs by navArgs()
+
+    @Inject
+    lateinit var application: BaseApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,13 +51,26 @@ class RecipeFragment : Fragment() {
 
                 val recipe = viewModel.recipe.value
 
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = recipe?.let {
-                            "Selected RecipeId: ${it.title}"
-                        } ?: "Loading...",
-                        fontSize = 21.sp,
-                    )
+                val scaffoldState = rememberScaffoldState()
+
+                AppTheme(darkTheme = application.isDark.value) {
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            if (loading && recipe == null) {
+                                Text("Loading...")
+                            } else {
+                                recipe?.let {
+                                    RecipeView(recipe = it)
+                                }
+                            }
+                            CircularIndeterminateProgressBar(isDisplayed = loading)
+                        }
+                    }
                 }
             }
         }
