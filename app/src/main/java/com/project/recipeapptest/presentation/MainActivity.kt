08353f5a61be2e2a8ee17c692f12a6026e2ffc3a
EhsanTgv <EhsanTgv@gmail.com>
2021-08-30 16:07:03 +0430

    Implement connectionChecker

--- app/src/main/java/com/project/recipeapptest/presentation/MainActivity.kt
+++ app/src/main/java/com/project/recipeapptest/presentation/MainActivity.kt
@@ -3,9 +3,12 @@ package com.project.recipeapptest.presentation
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import com.google.gson.GsonBuilder
 import com.project.recipeapptest.R
 import com.project.recipeapptest.network.RecipeService
+import com.project.recipeapptest.presentation.util.ConnectionLiveData
+import com.project.recipeapptest.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
@@ -15,8 +18,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var connectionLiveData: ConnectionLiveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        connectionLiveData = ConnectionLiveData(this)
+        connectionLiveData.observe(this, { isNetworkAvailable ->
+            Log.i(TAG, "isNetworkAvailable -> $isNetworkAvailable")
+        })
     }
 }
